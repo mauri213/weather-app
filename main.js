dce94d0a605b795552495990ab7b24ce9491b4c9
Mauricio Orozco <Mauricio Orozco>
2016-10-26 14:30:29 -0400

    ------------ Added main Menu and subMenu functions

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 function mainMenu () {
-
+	$.ajax({
+			url: 'http://api.openweathermap.org/data/2.5/find?lat=-37&lon=-118&cnt=10&appid=7cd032422ed7196011cec831381eb45b',
+			method: 'GET',
+			success: function (results) {
+				mainMenu(results.key);
+			}
+		});
+	}
 }
 
 function subMenu () {
-	
+
 }
